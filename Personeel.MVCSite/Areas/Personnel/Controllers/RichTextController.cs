cbb31b19824e534324d308472d48083eb76b88b1
liliangouw <1803443590@qq.com>
2022-04-12 21:34:02 +0800

    添加富文本编辑器，功能待完善

---
+++ Personeel.MVCSite/Areas/Personnel/Controllers/RichTextController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Personeel.BLL;
+using Personeel.DTO;
+
+namespace Personeel.MVCSite.Areas.Personnel.Controllers
+{
+    public class RichTextController : Controller
+    {
+        // GET: Personnel/RichText
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult SaveInfo()
+        {
+            string textValue = Request.Form["textContent"];
+            string textTitle = Request.Form["textTitle"];
+            string textDes = Request.Form["textDes"];
+            IBLL.IAnnounceManager announceManager = new AnnounceManager();
+            announceManager.AddAnnounce(textTitle, textDes, textValue, Guid.Parse(Session["userId"].ToString()));
+            return Content("save success");
+        }
+        //显示文章
+        public async Task<ActionResult> ViewE(Guid id)
+        {
+            IBLL.IAnnounceManager announceManager = new AnnounceManager();
+            AnnounceInfoDto info= await announceManager.GetOneAnnounce(id);
+            ViewBag.File = info.Text.ToString();
+            return View();
+        }
+    }
+}
